d4919b17d50b998cb3a7f6323683df95bbf0fa2d
y.kochergin <y.kochergin@MAC981.local>
2022-02-10 01:24:38 +0300

    Sprint_1

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path"
+
+const __dirname = path.resolve();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(PORT, function () {
+  console.log('App started..')
+});
